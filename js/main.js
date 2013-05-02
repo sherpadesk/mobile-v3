24c8c38e2e15c36ec90c5d4c10bb6fbbbb8df8c6
benpfohl <benpfohl@truenorthwebdesign.com>
2013-05-02 09:48:08 -0400

    Updated google analytics code

--- js/main.js
+++ js/main.js
@@ -40,7 +40,6 @@ var SherpaDesk = {
                 xhr.setRequestHeader('Authorization', 'Basic ' + btoa(header));
                 },
             url: url + method,
-            async: true,
             cache: false,
             data: data, 
             dataType: 'json'   
@@ -1185,5 +1184,13 @@ function logOut(){
 	};
 	
 // handlebars helpers
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	    })(window,document,'script','http://www.google-analytics.com/analytics.js','ga');
+	  
+	    ga('create', 'UA-998328-15', 'sherpadesk.com');
+	    ga('send', 'pageview');
 
 	
