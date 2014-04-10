9d81ea247b28eea25e2c4db50f59ea2dd1d298a0
Eugene <jtrue@mail.ru>
2014-04-10 17:26:32 +0300

    added chrome ext

--- beta/js/main.js
+++ beta/js/main.js
@@ -47,169 +47,6 @@ var clean_uri = location.protocol + "//" + location.host + location.pathname;
 window.history.replaceState({}, document.title, clean_uri);
 }
 
-var googleapi = {
-    setToken: function (data) {
-        //Cache the token
-        localStorage.access_token = data.access_token;
-        //Cache the refresh token, if there is one
-        localStorage.refresh_token = data.refresh_token || localStorage.refresh_token;
-        //Figure out when the token will expire by using the current
-        //time, plus the valid time (in seconds), minus a 1 minute buffer
-        var expiresAt = new Date().getTime() + parseInt(data.expires_in, 10) * 1000 - 60000;
-        localStorage.expires_at = expiresAt;
-    },
-    authorize: function (options) {
-        var deferred = $.Deferred();
-
-        //Build the OAuth consent page URL
-        var authUrl = 'https://accounts.google.com/o/oauth2/auth?' + $.param({
-            client_id: options.client_id,
-            redirect_uri: options.redirect_uri,
-            response_type: 'code',
-            scope: options.scope,
-            access_type : "offline"
-        });
-
-        //Open the OAuth consent page in the InAppBrowser
-        var authWindow = window.open(authUrl, '_blank', 'location=no,toolbar=no');
-
-        //The recommendation is to use the redirect_uri "urn:ietf:wg:oauth:2.0:oob"
-        //which sets the authorization code in the browser's title. However, we can't
-        //access the title of the InAppBrowser.
-        //
-        //Instead, we pass a bogus redirect_uri of "http://localhost", which means the
-        //authorization code will get set in the url. We can access the url in the
-        //loadstart and loadstop events. So if we bind the loadstart event, we can
-        //find the authorization code and close the InAppBrowser after the user
-        //has granted us access to their data.
-        $(authWindow).on('loadstart', function (e) {
-            var url = e.originalEvent.url;
-            var code = /\?code=(.+)$/.exec(url);
-            var error = /\?error=(.+)$/.exec(url);
-
-            if (code || error) {
-                //Always close the browser when match is found
-                authWindow.close();
-            }
-
-            if (code) {
-                //Exchange the authorization code for an access token
-                $.post('https://accounts.google.com/o/oauth2/token', {
-                    code: code[1],
-                    client_id: options.client_id,
-                    client_secret: options.client_secret,
-                    redirect_uri: options.redirect_uri,
-                    grant_type: 'authorization_code'
-                }).done(function (data) {
-                    googleapi.setToken(data);
-                    deferred.resolve(data);
-                }).fail(function (response) {
-                    deferred.reject(response.responseJSON);
-                });
-            } else if (error) {
-                //The user denied access to the app
-                deferred.reject({
-                    error: error[1]
-                });
-            }
-        });
-
-        return deferred.promise();
-    },
-
-    openAuthorizeSite: function (options) {
-
-        //Build the OAuth consent page URL
-        var authUrl = 'https://accounts.google.com/o/oauth2/auth?' + $.param({
-            client_id: options.client_id,
-            redirect_uri: options.redirect_uri,
-            response_type: 'code',
-            scope: options.scope,
-            access_type: "offline"
-        });
-
-        //Open the OAuth consent page in the InAppBrowser
-        document.location.href = authUrl;
-    },
-
-    authorizeSite: function (options) {
-        var deferred = $.Deferred();
-
-        //The recommendation is to use the redirect_uri "urn:ietf:wg:oauth:2.0:oob"
-        //which sets the authorization code in the browser's title. However, we can't
-        //access the title of the InAppBrowser.
-        //
-        //Instead, we pass a bogus redirect_uri of "http://localhost", which means the
-        //authorization code will get set in the url. We can access the url in the
-        //loadstart and loadstop events. So if we bind the loadstart event, we can
-        //find the authorization code and close the InAppBrowser after the user
-        //has granted us access to their data.
-        
-            var url = window.location.search;
-            var code = /\?code=(.+)$/.exec(url);
-            var error = /\?error=(.+)$/.exec(url);
-
-            if (code || error) {
-                //Always close the browser when match is found
-                cleanQuerystring();
-            }
-
-            if (code) {
-                //Exchange the authorization code for an access token
-                $.post('https://accounts.google.com/o/oauth2/token', {
-                    code: code[1],
-                    client_id: options.client_id,
-                    client_secret: options.client_secret,
-                    redirect_uri: options.redirect_uri,
-                    grant_type: 'authorization_code'
-                }).done(function (data) {
-                    alert(JSON.stringify(data));
-                    googleapi.setToken(data);
-                    deferred.resolve(data);
-                }).fail(function (response) {
-                    alert(JSON.stringify(response));
-                    deferred.reject(response.responseJSON);
-                });
-            } else if (error) {
-                //The user denied access to the app
-                deferred.reject({
-                    error: error[1]
-                });
-            }
-
-        return deferred.promise();
-    },
-    getToken: function (options) {
-        var deferred = $.Deferred();
-
-        if (new Date().getTime() < localStorage.expires_at) {
-            deferred.resolve({
-                access_token: localStorage.access_token
-            });
-        } else if (localStorage.refresh_token) {
-            $.post('https://accounts.google.com/o/oauth2/token', {
-                refresh_token: localStorage.refresh_token,
-                client_id: options.client_id,
-                client_secret: options.client_secret,
-                grant_type: 'refresh_token'
-            }).done(function (data) {
-                googleapi.setToken(data);
-                deferred.resolve(data);
-            }).fail(function (response) {
-                alert(JSON.stringify(response));
-                deferred.reject(response.responseJSON);
-            });
-        } else {
-            deferred.reject();
-        }
-
-        return deferred.promise();
-    },
-    userInfo: function (options) {
-        return $.getJSON('https://www.googleapis.com/oauth2/v1/userinfo', options);
-    }
-};
-
 // ---- SHERPADESK OBJECT LEGEND -----
 // 01 - init - Initialization
 // 02 - getSherpaDesk - Global ajax request
@@ -364,44 +201,6 @@ var SherpaDesk = {
 		
 		},
 		
-		//
-	getGoogleUserInfo: function (configPass) {
-
-        //Get the token, either from the cache
-        //or by using the refresh token.
-        googleapi.getToken({
-            client_id: this.client_id,
-            client_secret: this.client_secret
-        }).then(function(data) {
-            //Pass the token to the API call and return a new promise object
-            return googleapi.userInfo({ access_token: data.access_token });
-        }).done(function(user) {
-            //Display a greeting if the API call was successful
-            if (user.email)
-                SherpaDesk.getLogin(configPass, user.email, localStorage.access_token);
-                $("body").empty().addClass('spinner');
-                SherpaDesk.init();  
-        }).fail(function() {
-            //If getting the token fails, or the token has been
-            //revoked, show the login view.
-            SherpaDesk.showLogin();
-            checkLogin(configPass);
-            loginWithGoogle(configPass);
-        });
-    },
-		
-		//Redirect to google oauth
-	onLoginButtonClick: function (configPass) {
-        //Show the consent page
-	    googleapi.openAuthorizeSite({
-	        client_id: this.client_id,
-	        client_secret: this.client_secret,
-	        redirect_uri: this.redirect_uri,
-	        scope: this.scope,
-	        access_type: "offline"
-	    });
-    },
-	
 	//Set Config Options
 	getConfig: function(configPass){
 		SherpaDesk.getSherpaDesk(configPass, 'config')
@@ -1695,6 +1494,13 @@ function checkLogin(configPass){
 			};
 	});
 	$('form.google_openid').get(0).setAttribute('action', ApiSite + 'api/auth/googleopenid');
+    $('a.sign-in-with-google').on('click', function(e){
+        e.preventDefault();
+        if (window.self !== window.top) {
+            $('form.google_openid').get(0).setAttribute('target', '_blank'); 
+        }
+        $('form.google_openid').get(0).submit();
+    });
 	};	
 			
 // Set the current Role in header filter
@@ -2190,7 +1996,12 @@ function logOut(){
 };
 
 var GooglelogOut = function () {
-    document.location.href = "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=" + MobileSite;
+    var logoutUrl = "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=" + MobileSite;
+    if (window.self === window.top) {
+        document.location.href = logoutUrl;
+    } else {
+        window.open(logoutUrl, "windowname1", 'width=300, height=600');
+    }
 }
 
 // Change Orgs / Inst
