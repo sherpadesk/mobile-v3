6571f42a0102c13994ca2139b39c1021b49ae1db
Matthew Bennett <mbennett24@student.gsu.edu>
2014-09-08 10:50:50 -0400

    bug fix
    Increased length of text before eclipse on tickets, queues, and accounts

--- js/main.js
+++ js/main.js
@@ -375,9 +375,9 @@ $(document).ready(function(){
                         //the key for this specific ticket
                         var data = returnData[i].key;
                         // ensure ticket subject length is not to long to be displayed (subject is elipsed if it is)
-                        if(subject.length > 19)
+                        if(subject.length > 25)
                         {
-                            subject = subject.substring(0,16)+"...";
+                            subject = subject.substring(0,23)+"...";
                         }
                         // ensure ticket initial post length is not to long to be displayed (initial post is elipsed if it is)
                         if(initialPost.length > 50)
@@ -698,7 +698,7 @@ $(document).ready(function(){
     function fillClasses(classResults, element, initialValue)
     {
         if(!isClass) $(''+element).parent().parent().hide();
-        fillSelect(classResults, element, initialValue, "Class: ");
+        fillSelect(classResults, element, initialValue, "");
 
         //Listen for class and detect sub-classes
         $(''+element).on('change', function(){
@@ -757,7 +757,7 @@ $(document).ready(function(){
                     console.log(returnData);
                     // get list of accounts add them to option select list
                     $("#addTicketAccounts").empty();
-                    fillSelect(returnData, "#addTicketAccounts", "<option value=0 disabled selected>Account</option>");
+                    fillSelect(returnData, "#addTicketAccounts", "<option value=0 disabled selected>choose an account</option>");
                     reveal();
                 }, function() {
                     console.log("fail @ ticket accounts");
@@ -770,7 +770,7 @@ $(document).ready(function(){
                 console.log(returnData);
                 // add techs to option select list
                 fillSelect(returnData, "#addTicketTechs",
-                           "<option value=0 disabled selected>Tech</option>", "",
+                           "<option value=0 disabled selected>choose a tech</option>", "",
                            "firstname,lastname");
                 reveal();
             },
@@ -784,7 +784,7 @@ $(document).ready(function(){
             var classes = getApi('classes');
             classes.done(
                 function(classResults){
-                    fillClasses(classResults, "#classTicketOptions", "<option value=0 disabled selected>Class</option>");
+                    fillClasses(classResults, "#classTicketOptions", "<option value=0 disabled selected>choose a class</option>");
                 });
 
 
@@ -1111,6 +1111,8 @@ $(document).ready(function(){
                     $("#taskTypes").empty();
                     // add task types to list
                     fillSelect(returnData, "#taskTypes");
+                    var chooseTask = '<option value=0>choose a project</option>';
+                    $(chooseTask).prependTo('#taskTypes');
                     reveal();
                 },
                 function() {
@@ -1134,7 +1136,7 @@ $(document).ready(function(){
                 success: function(returnData) {
                     console.log(returnData);
                     $("#timeAccounts").empty();
-                    var chooseAccount = "<option value=0>Choose An Account</option>";
+                    var chooseAccount = "<option value=0>choose an account</option>";
                     $(chooseAccount).appendTo("#timeAccounts");
                     // accounts to add time
                     for(var i = 0; i < returnData.length; i++)
@@ -1144,8 +1146,9 @@ $(document).ready(function(){
                         var insert = "<option value="+value+">"+task+"</option>";
                         $(insert).appendTo("#timeAccounts");
                     }
-                    var chooseProject = "<option value=0>Choose a Project</option>";
+                    var chooseProject = "<option value=0>choose a project</option>";
                     $(chooseProject).appendTo("#timeProjects");
+
 
                 },
                 complete:function(){
@@ -2013,9 +2016,9 @@ $(document).ready(function(){
                         var subject = returnData[i].subject;
                         var data = returnData[i].key;
                         //check subject length can be displayed correctly
-                        if(subject.length > 19)
+                        if(subject.length > 25)
                         {
-                            subject = subject.substring(0,16)+"...";
+                            subject = subject.substring(0,23)+"...";
                         }
                         //check intial post length can be displayed correctly
                         if(intialPost.length > 100)
@@ -2174,9 +2177,9 @@ $(document).ready(function(){
                         var subject = returnData[i].subject;
                         var data = returnData[i].key;
                         //check subject length can be displayed correctly
-                        if(subject.length > 19)
+                        if(subject.length > 25)
                         {
-                            subject = subject.substring(0,16)+"...";
+                            subject = subject.substring(0,23)+"...";
                         }
                         //check intial post length can be displayed correctly
                         if(intialPost.length > 100)
@@ -2220,9 +2223,9 @@ $(document).ready(function(){
                         var subject = returnData[i].subject;
                         var data = returnData[i].key;
                         //check subject length
-                        if(subject.length > 19)
+                        if(subject.length > 25)
                         {
-                            subject = subject.substring(0,16)+"...";
+                            subject = subject.substring(0,23)+"...";
                         }
                         //check initial post length
                         if(intialPost.length > 100)
@@ -2267,9 +2270,9 @@ $(document).ready(function(){
                         var intialPost = returnData[i].initial_post;
                         var subject = returnData[i].subject;
                         var data = returnData[i].key;
-                        if(subject.length > 19)
+                        if(subject.length > 25)
                         {
-                            subject = subject.substring(0,16)+"...";
+                            subject = subject.substring(0,23)+"...";
                         }
                         if(intialPost.length > 100)
                         {
@@ -2313,9 +2316,9 @@ $(document).ready(function(){
                         var intialPost = returnData[i].initial_post;
                         var subject = returnData[i].subject;
                         var data = returnData[i].key;
-                        if(subject.length > 14)
+                        if(subject.length > 25)
                         {
-                            subject = subject.substring(0,11)+"...";
+                            subject = subject.substring(0,23)+"...";
                         }
                         if(intialPost.length > 100)
                         {
@@ -2384,8 +2387,8 @@ $(document).ready(function(){
                     for(var i = 0; i < returnData.length; i++)
                     {
                         name = returnData[i].name;
-                        if(name.length > 16){
-                            name = name.substring(0,14)+"...";
+                        if(name.length > 24){
+                            name = name.substring(0,21)+"...";
                         }
                         // check the number of open tickets for the account if the number of tickets is greater than 100 sub 99+
                         var openTks = returnData[i].account_statistics.ticket_counts.open;
@@ -2471,9 +2474,9 @@ $(document).ready(function(){
                             hours = hours+".00";
                         }
                         // check text length
-                        if(text.length > 18)
+                        if(text.length > 24)
                         {
-                            text = text.substring(0,18)+"...";
+                            text = text.substring(0,21)+"...";
                         }
                         var nameCheck = returnData[i].user_name;
                         if(nameCheck.length > 15)
@@ -2576,9 +2579,9 @@ $(document).ready(function(){
                         //the key for this specific ticket
                         var data = returnData[i].key;
                         // ensure ticket subject length is not to long to be displayed (subject is elipsed if it is)
-                        if(subject.length > 19)
+                        if(subject.length > 25)
                         {
-                            subject = subject.substring(0,16)+"...";
+                            subject = subject.substring(0,23)+"...";
                         }
                         // ensure ticket initial post length is not to long to be displayed (initial post is elipsed if it is)
                         if(initialPost.length > 50)
