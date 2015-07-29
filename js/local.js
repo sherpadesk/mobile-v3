d6389e2248e57acfff120e5855c98cc476718cf7
Eugene <jtrue@mail.ru>
2015-07-29 14:48:50 +0300

    added files

---
+++ js/local.js
@@ -0,0 +1,13 @@
+/*jshint -W004, -W041, -W103, eqeqeq: false, noempty: false, undef: false, latedef: false, eqnull: true, multistr: true*/
+/*global jQuery, $ */
+
+var Page = location.href.match(/(.+\w\/)(.+)/)[2];
+$( window ).unload(function() { localStorage.setItem("referrer", Page); });
+
+var isExtension = false;
+
+function default_redirect (is_tech)
+{
+    //window.location = is_tech ? "dashboard.html" : "ticket_list.html";
+}
+
---
+++ js/server.js
@@ -0,0 +1,13 @@
+/*jshint -W004, -W041, -W103, eqeqeq: false, noempty: false, undef: false, latedef: false, eqnull: true, multistr: true*/
+/*global jQuery, $ */
+
+var Page = location.pathname.substr(1);
+
+var isExtension = window.self !== window.top;
+if (isExtension) localStorage.setItem("referrer", Page);
+
+function default_redirect (is_tech)
+{
+    //window.location = is_tech ? "dashboard.html" : "ticket_list.html";
+}
+
