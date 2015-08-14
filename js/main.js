a0c338d7f3b07443e769967888a1494886fad9a4
Nastya <nastya_01.88@mail.ru>
2015-08-14 17:56:55 +0300

    #1365: View site design (correcting design page Todos)

--- js/main.js
+++ js/main.js
@@ -3280,7 +3280,7 @@ $(document).ready(function(){
             date = formatDate(returnData[i].due_date);
                 }
                             
-                        var log = "<ul id='recipHeader' class='invoiceAccount'><li>"+ticket_string+"</li><li id='addRecipient' class='detail3Short'><!--img class='addIcon' src='img/check.png'--></li></ul><div class='styledSelectToDos ToDosMain'>"+ returnData[i].name+"</div><ul id='recipientList' class='recipientToDos dots'><h4><p class='ToDos'><input class='timeTodos' type='checkbox' value=''>"+ returnData[i].text+"</p></h4><p class='ToDosData'>"+ returnData[i].assigned_name+ " "+date+"</p></div></ul>";
+                        var log = "<ul id='recipHeader' class='lineTodos'><li>"+ticket_string+"</li><li id='addRecipient' class='detail3Short'><!--img class='addIcon' src='img/check.png'--></li></ul><div class='styledSelectToDos ToDosMain'>"+ returnData[i].name+"</div><ul id='recipientList' class='recipientToDos dots'><h4><p class='ToDos'><input class='timeTodos' type='checkbox' value=''>"+ returnData[i].text+"</p></h4><p class='ToDosData'>"+ returnData[i].assigned_name+ " "+date+"</p></div></ul>";
                         
                         
                          $(log).appendTo("#todoList");
