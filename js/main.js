6a14331a66aaad079121a0887470bc3181cfacca
Eugene <jtrue@mail.ru>
2015-11-12 21:52:20 +0200

    fixed project

--- js/main.js
+++ js/main.js
@@ -2156,7 +2156,7 @@ $(document).ready(function(){
                         var projects = getApi('projects');
                         projects.done(
                             function(projectResults){
-                                fillSelect(projectResults, "#ticketProject", "<option selected value='null' disabled=disabled>choose project</option>");
+                                fillSelect(projectResults, "#ticketProject", "<option selected value='null'>choose project</option>");
                                 $("#ticketProject").val(returnData.project_id).trigger("change");
                                 }
                         );
