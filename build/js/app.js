9fa04b9975b4b4226950d066180c0df03b969f19
Eugene <jtrue@mail.ru>
2017-02-20 11:44:30 +0200

    updated speed

--- build/js/app.js
+++ build/js/app.js
@@ -1,3 +1,4 @@
+var stimer;
 
 function downloadJSAtOnload() {
 
@@ -14,17 +15,10 @@ function downloadJSAtOnload() {
   }
 
   var dash_cache = localStorage.dash_cache || '<ion-loading role="dialog" class="loading-cmp"><div class="backdrop" disable-activated="" role="presentation"></div><div class="loading-wrapper loading-wrapper1" style="opacity: 1;  transform: scale(1);"><div class="loading-spinner"><img src="img/loading2.gif"></div><div class="loading-content">Loading Your Profile...</div></div></ion-loading><ion-nav id="nav" swipe-back-enabled="false" class="menu-content menu-content-reveal has-views" style="touch-action: pan-y; -webkit-user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0); transform: translateX(0px);"><div></div><ion-page _ngcontent-tcs-14="" class="login-page show-page" style="z-index: 99;"><ion-content class="login" padding=""><scroll-content><div class="list max-width"><a title="Support Portal" href="https://support.sherpadesk.com/portal/"><img class="imglogo img-padding" src="img/logo.png"></a><form novalidate=""><div class="tooltips"><input disabled class="width100 blue3 subject-create commentText ng-untouched ng-pristine ng-valid" ngcontrol="email" pattern="^[^@\s]+@[^@\s]+(\.[^@\s]+)+$" placeholder="email" required="" type="email"> <!--template bindings={}--></div><br><div class="tooltips"><input disabled class="width100 blue3 subject-create commentText ng-untouched ng-dirty ng-valid" ngcontrol="password" placeholder="password" required="" type="password"> <!--template bindings={}--></div><br><button block="" class="login-margin disable-hover button button-default button-block button-secondary" secondary="" type="submit"><span class="button-inner">Login</span><ion-button-effect></ion-button-effect></button><br></scroll-content></ion-content></ion-page><div nav-portal=""></div></ion-nav>';
-  if (temp && temp[0] && dash_cache)
-    temp[0].innerHTML = loading2 + dash_cache;
-
-  temp = null;
-  dash_cache = null;
-  loading2 = null;
-
 
   var element;
 
-  if (localStorage.getItem("isPhonegap") === "true"){
+  if (localStorage.getItem("isPhonegap") === "true" && localStorage.getItem("version") == "24"){
     element = document.createElement("script");
     element.src = "build/js/cordova.js";
     document.body.appendChild(element);
@@ -46,6 +40,11 @@ function downloadJSAtOnload() {
   element.src = "build/js/zone.min.js";
   document.body.appendChild(element);
 
+  element = document.createElement( "link" );
+  element.href = "http://m.sherpadesk.com/build/css/app.ios.css";
+  element.rel = "stylesheet";
+  document.body.appendChild(element);
+
   element = document.createElement("script");
   element.src = "build/js/vendor.bundle.js";
   document.body.appendChild(element);
@@ -53,15 +52,33 @@ function downloadJSAtOnload() {
   element = document.createElement("script");
   element.src = "http://m.sherpadesk.com/build/js/app2.bundle.js";
 
-  var interval = setInterval(function(){
-    if (window.test2){
-      clearInterval(interval);
+  setTimeout( function(){
+document.body.appendChild(element);
+element = null;
+}, 100);
 
-      document.body.appendChild(element);
-      element = null;
-      test2 = null;
+  if (temp && temp[0] && dash_cache)
+    stimer = setInterval( function(){
+      if (checksloaded())
+      {
+          clearInterval(stimer);
+    temp[0].innerHTML = loading2 + dash_cache;
+      temp = null;
+  dash_cache = null;
+  loading2 = null;
+}
+}, 200);
+
+}
+
+function checksloaded(){
+    var ss = document.styleSheets;
+    for (var i = 0, max = ss.length; i < max; i++) {
+        if (ss[i].href == "http://m.sherpadesk.com/build/css/app.ios.css"){
+          return true;
+        }
     }
-  }, 200);
+    return false;
 }
 
 if (window.addEventListener)
