c6bd9adeaf1027e073526163b29b0ae02fe5493d
Eugene <jtrue@mail.ru>
2017-02-21 21:01:09 +0200

    fixed

--- build/js/app.js
+++ build/js/app.js
@@ -3,90 +3,112 @@ var stimer, vtimer;
 function downloadJSAtOnload() {
 
   var temp = document.getElementsByTagName("ion-app1");
-  var loading2 = '';
-  if (!localStorage.current || (localStorage.current.indexOf("\"instance\"") - localStorage.current.indexOf("\"key\"") < 56))
-    localStorage.dash_cache = "";
-  else
-  {
-    loading2 = '<img id=preload class=Absolute-Center src=img/loading2.gif alt="Loading...">';
-    var a=new XMLHttpRequest();a.open("GET","http://api.sherpadesk.com/ping",!0);
-    var c = JSON.parse(localStorage.getItem("current") || "null") || {};
-    a.setRequestHeader("Authorization","Basic "+ btoa(c.org+"-"+c.inst+":"+c.key));a.send();
+  
+  if (localStorage.isPhonegap == "true"){
+
+  // Create the Style Element
+  var styleElem = document.createElement('style');
+  styleElem.type = 'text/css' ;
+
+  var css = 'ion-app1 scroll-content {padding-top: 36px  !important;} ion-app1 ion-navbar {top: 20px  !important;} ' ;
+
+  if(styleElem.styleSheet){
+    styleElem.styleSheet.cssText = css;
+  }
+  else{
+    styleElem.appendChild(document.createTextNode(css));
   }
 
-  var dash_cache = localStorage.dash_cache || '<ion-loading role="dialog" class="loading-cmp"><div class="backdrop" disable-activated="" role="presentation"></div><div class="loading-wrapper loading-wrapper1" style="opacity: 1;  transform: scale(1);"><div class="loading-spinner"><img src="img/loading2.gif"></div><div class="loading-content">Loading Your Profile...</div></div></ion-loading><ion-nav id="nav" swipe-back-enabled="false" class="menu-content menu-content-reveal has-views" style="touch-action: pan-y; -webkit-user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0); transform: translateX(0px);"><div></div><ion-page _ngcontent-tcs-14="" class="login-page show-page" style="z-index: 99;"><ion-content class="login" padding=""><scroll-content><div class="list max-width"><a title="Support Portal" href="https://support.sherpadesk.com/portal/"><img class="imglogo img-padding" src="img/logo.png"></a><form novalidate=""><div class="tooltips"><input disabled class="width100 blue3 subject-create commentText ng-untouched ng-pristine ng-valid" ngcontrol="email" pattern="^[^@\s]+@[^@\s]+(\.[^@\s]+)+$" placeholder="email" required="" type="email"> <!--template bindings={}--></div><br><div class="tooltips"><input disabled class="width100 blue3 subject-create commentText ng-untouched ng-dirty ng-valid" ngcontrol="password" placeholder="password" required="" type="password"> <!--template bindings={}--></div><br><button block="" class="login-margin disable-hover button button-default button-block button-secondary" secondary="" type="submit"><span class="button-inner">Login</span><ion-button-effect></ion-button-effect></button><br></scroll-content></ion-content></ion-page><div nav-portal=""></div></ion-nav>';
+// Append the Style element to the Head
+var head = document.getElementsByTagName('head')[0] ;
+head.appendChild(styleElem);
+}
 
-  var element;
 
-  if (localStorage.getItem("isPhonegap") === "true"){
-    element = document.createElement("script");
-    element.src = "build/js/cordova.js";
-    document.body.appendChild(element);
-  }
+var loading2 = '';
+if (!localStorage.current || (localStorage.current.indexOf("\"instance\"") - localStorage.current.indexOf("\"key\"") < 56))
+  localStorage.dash_cache = "";
+else
+{
+  loading2 = '<img id=preload class=Absolute-Center src=img/loading2.gif alt="Loading...">';
+  var a=new XMLHttpRequest();a.open("GET","http://api.sherpadesk.com/ping",!0);
+  var c = JSON.parse(localStorage.getItem("current") || "null") || {};
+  a.setRequestHeader("Authorization","Basic "+ btoa(c.org+"-"+c.inst+":"+c.key));a.send();
+}
 
+var dash_cache = localStorage.dash_cache || '<ion-loading role="dialog" class="loading-cmp"><div class="backdrop" disable-activated="" role="presentation"></div><div class="loading-wrapper loading-wrapper1" style="opacity: 1;  transform: scale(1);"><div class="loading-spinner"><img src="img/loading2.gif"></div><div class="loading-content">Loading Your Profile...</div></div></ion-loading><ion-nav id="nav" swipe-back-enabled="false" class="menu-content menu-content-reveal has-views" style="touch-action: pan-y; -webkit-user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0); transform: translateX(0px);"><div></div><ion-page _ngcontent-tcs-14="" class="login-page show-page" style="z-index: 99;"><ion-content class="login" padding=""><scroll-content><div class="list max-width"><a title="Support Portal" href="https://support.sherpadesk.com/portal/"><img class="imglogo img-padding" src="img/logo.png"></a><form novalidate=""><div class="tooltips"><input disabled class="width100 blue3 subject-create commentText ng-untouched ng-pristine ng-valid" ngcontrol="email" pattern="^[^@\s]+@[^@\s]+(\.[^@\s]+)+$" placeholder="email" required="" type="email"> <!--template bindings={}--></div><br><div class="tooltips"><input disabled class="width100 blue3 subject-create commentText ng-untouched ng-dirty ng-valid" ngcontrol="password" placeholder="password" required="" type="password"> <!--template bindings={}--></div><br><button block="" class="login-margin disable-hover button button-default button-block button-secondary" secondary="" type="submit"><span class="button-inner">Login</span><ion-button-effect></ion-button-effect></button><br></scroll-content></ion-content></ion-page><div nav-portal=""></div></ion-nav>';
+
+var element;
+
+if (localStorage.getItem("isPhonegap") === "true"){
   element = document.createElement("script");
-  element.src = "build/js/es6-shim.min.js";
+  element.src = "build/js/cordova.js";
   document.body.appendChild(element);
+}
 
-  element = document.createElement("script");
-  element.src = "build/js/shims_for_IE.js";
-  document.body.appendChild(element);
+element = document.createElement("script");
+element.src = "build/js/es6-shim.min.js";
+document.body.appendChild(element);
 
-  element = document.createElement("script");
-  element.src = "build/js/Reflect.js";
-  document.body.appendChild(element);
+element = document.createElement("script");
+element.src = "build/js/shims_for_IE.js";
+document.body.appendChild(element);
 
-  element = document.createElement("script");
-  element.src = "build/js/zone.min.js";
-  document.body.appendChild(element);
+element = document.createElement("script");
+element.src = "build/js/Reflect.js";
+document.body.appendChild(element);
 
-  element = document.createElement( "link" );
-  element.href = "http://m.sherpadesk.com/build/css/app.ios.css";
-  element.rel = "stylesheet";
-  document.body.appendChild(element);
+element = document.createElement("script");
+element.src = "build/js/zone.min.js";
+document.body.appendChild(element);
 
-  element = document.createElement("script");
-  element.src = "build/js/vendor.bundle.js";
-  document.body.appendChild(element);
+element = document.createElement( "link" );
+element.href = "http://m.sherpadesk.com/build/css/app.ios.css";
+element.rel = "stylesheet";
+document.body.appendChild(element);
 
-  element = null;
+element = document.createElement("script");
+element.src = "build/js/vendor.bundle.js";
+document.body.appendChild(element);
+
+element = null;
 
 setTimeout(function(){
   vtimer = setInterval(reloadScript, 1000);
 }, localStorage.isPhonegap !== "true" ? 500 : 200);
 
-  if (temp && temp[0] && dash_cache)
-    stimer = setInterval( function(){
-      if (checksloaded())
-      {
-          clearInterval(stimer);
-    temp[0].innerHTML = loading2 + dash_cache;
+if (temp && temp[0] && dash_cache)
+  stimer = setInterval( function(){
+    if (checksloaded())
+    {
+      clearInterval(stimer);
+      temp[0].innerHTML = loading2 + dash_cache;
       temp = null;
-  dash_cache = null;
-  loading2 = null;
-}
-}, 500);
+      dash_cache = null;
+      loading2 = null;
+    }
+  }, 500);
 
 }
 
 function reloadScript()
 {
   if (!window.t1){
-  var element1 = document.createElement("script");
-  element1.src = "http://m.sherpadesk.com/build/js/app.bundle.js";
-  document.body.appendChild(element1);
-  element1 = null;
-}
+    var element1 = document.createElement("script");
+    element1.src = "http://m.sherpadesk.com/build/js/app.bundle.js";
+    document.body.appendChild(element1);
+    element1 = null;
+  }
 }
 
 function checksloaded(){
-    var ss = document.styleSheets;
-    for (var i = 0, max = ss.length; i < max; i++) {
-        if (ss[i].href == "http://m.sherpadesk.com/build/css/app.ios.css"){
-          return true;
-        }
+  var ss = document.styleSheets;
+  for (var i = 0, max = ss.length; i < max; i++) {
+    if (ss[i].href == "http://m.sherpadesk.com/build/css/app.ios.css"){
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 if (window.addEventListener)
